0902b7fdf5540596c435e15e6b0e871023e244b4
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-07 14:05:25 -0700

    Help me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main-prints puts output
+ * main - print using puts
  *
- * return-zero 
- *
+ * Return: always 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
