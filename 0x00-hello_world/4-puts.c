1a77db1130ad7947fe561e54b88e7cf976b5ffba
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-07 13:47:14 -0700

    HMMM

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 /**
- * main:
- * prints puts output
+ * main-prints puts output
  *
- * return:
- * zero on success always
+ * return-zero 
  *
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
