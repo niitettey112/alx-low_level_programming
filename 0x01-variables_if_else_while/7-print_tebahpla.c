74352fe28fc96aae58a2be71d80085c0ffa2f57d
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-08 14:39:57 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- Print lower case alphabets in reverse
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char alpha_rev;
+
+	for (alpha_rev = 'z'; alpha_rev > 'a'; alpha_rev--)
+	{
+		putchar(alpha_rev);
+	}
+	putchar('\n');
+	return (0);
+}
