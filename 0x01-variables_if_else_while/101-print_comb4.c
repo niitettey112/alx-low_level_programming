3c002e9404b7d1500cb8f250afadfd7a8247aec2
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-08 15:37:37 -0700

    combinations again

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main- combination again
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int a,b,c;
+
+	for (a = 48; a <= 55; a++)
+	{
+		for (b = a + 1; b <= 56; b++)
+		{
+			for (c = b + 1; c <= 57; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a < 55)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
