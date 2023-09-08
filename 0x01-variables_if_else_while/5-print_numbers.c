d8e78552073b6ffe4d22b81e3ad997f3321e19c9
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-08 13:08:58 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- entry point of program
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
