55f0d8bdeaa1bfc5a2fad0471cc890656f49fa06
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-08 09:30:52 -0700

    Positive anything

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main: Entry point
+ * main- Entry point
  *
  * Return:0
  */
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 		if (n > 0)
 		{
-			printf("%d is  positve\n", n);
+			printf("%d is positve\n", n);
 		}
 		else if (n == 0)
 		{
@@ -23,7 +23,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d negative number\n", n);
+			printf("%d is negative\n", n);
 		}
 	return (0);
 }
