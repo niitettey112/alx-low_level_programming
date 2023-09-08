eaa0504058c9039d48e8088c968d0eec409a47e1
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-08 15:53:14 -0700

    LAst combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,7 @@ int main(void)
 				putchar(' ');
 				putchar((b / 10) + 48);
 				putchar((b % 10) + 48);
-				if (i != 98 || j != 99)
+				if (a != 98 || b != 99)
 				{
 					putchar(',');
 					putchar(' ');
