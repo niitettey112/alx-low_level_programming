3ff7c1ac5cc20a796ec641b461331c3a78e4eecd
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-08 15:51:56 -0700

    LAst combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main- combination again and again
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			if (a < b)
+			{
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
