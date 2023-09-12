b2becb655955882b2dbe21976f8cc18e3f52e6e1
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-12 16:28:20 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function to add two numbers
+ *
+ * @n: First number
+ * @m: second number
+ *
+ * Return: addition of @n and @m
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
+
