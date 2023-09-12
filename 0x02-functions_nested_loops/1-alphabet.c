0858584d304338c2a1393190f164faa2aa99f74d
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-12 12:01:38 -0700

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print alphabets
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
