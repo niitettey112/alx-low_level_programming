a3a9de74f4f80d305e0b3936a83ceaea16da501f
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-14 15:33:50 -0700

    Straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,6 @@
 
 void print_line(int n)
 {
-	int i;
 
 	if (n <= 0)
 	{
@@ -19,7 +18,7 @@ void print_line(int n)
 	{
 		int i;
 
-		for (i = 1; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
