f1479859485784d78417c4463d9548407fcc23cd
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-14 12:41:48 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * print_numbers - function to print numbers
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
