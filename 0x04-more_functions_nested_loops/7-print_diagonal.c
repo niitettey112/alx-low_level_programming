2a18ae8b421a48fd6c46c48395497a2eab22e021
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-14 15:45:00 -0700

    parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to print diagonal lines
+ * @n: number of times the line should be printed
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a, b;
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b <= a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
