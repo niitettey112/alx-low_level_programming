84da6c29478c89cf6dfbe80560841b8c19b81802
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-14 16:40:41 -0700

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factors.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - print the largest number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long i, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (i = 1; i <= square; i++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%ld\n", maxf);
+
+	return (0);
+}
