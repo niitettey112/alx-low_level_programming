be62d46999f3001973af5e2611163808334cdfc0
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-18 16:15:47 -0700

    Print arrays

---
+++ 0x05-pointers_arrays_strings/8-print_arrays.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - function to print n number of array of integers
+ * @a: array
+ * @n: n elements
+ *
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int x = 0;
+
+	for (; x < n; x++)
+	{
+		printf("%d", *(a + x));
+		if (a != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
