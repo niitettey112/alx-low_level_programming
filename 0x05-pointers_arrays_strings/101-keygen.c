0013a1f2884cb71b237545e1909b208ce93eddc9
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-18 16:48:46 -0700

    Keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program to generate password
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int pass[100];
+	int a, sum, b;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (a = 0; a < 100; a++)
+	{
+		pass[a] = rand() % 78;
+		sum += (pass[a] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			b = 2772 - sum - '0';
+			sum += b;
+			putchar(b + '0');
+			break;
+		}
+	}
+	return (0);
+}
