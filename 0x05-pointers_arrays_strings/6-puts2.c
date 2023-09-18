93665580d6207aa0f56d178c77f776854f21a571
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-18 15:43:01 -0700

    Put two strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print every other character of a string
+ * @str: parameter
+ *
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int counter  = 0;
+
+	while (counter >= 0)
+	{
+		if (str[counter] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (counter % 2 == 0)
+		{
+			_putchar(str[counter]);
+		}
+		count++;
+	}
+}
