a40d372eaa86dd0e308f43984b87af1eb6021198
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-22 08:10:53 -0700

    Look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - function to change letters to uppercase
+ * @a: parameter
+ *
+ * Return: n;
+ */
+char *string_toupper(char *a)
+{
+	int n = 0;
+
+	while (a[n] != '\0')
+	{
+		if (a[n] >= 'a' && a[n] <= 'z')
+		{
+			a[n] = a[n] - 32;
+		}
+	}
+	return (n);
+}
