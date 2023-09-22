ef61d2960f5b18225c6bd2580539d4d65624b558
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-22 05:52:55 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,6 +11,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(src, dest);
+	int a, b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	b = 0;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+
+	dest[a] = '\0';
 	return (dest);
 }
