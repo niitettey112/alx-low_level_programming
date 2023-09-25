b8e38939266283236a2894b9c54d952ce535d697
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-25 15:30:49 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - entry
+ * @s: input
+ * @c: input
+ *
+ * Return: NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
