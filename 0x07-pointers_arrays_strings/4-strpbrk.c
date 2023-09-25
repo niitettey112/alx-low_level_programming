34cc4f3c2af8a3c025c0b9d7a963490acf8a7d48
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-25 15:58:39 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function to search a string for any of set of bytes
+ * @s: input
+ * @accept: input
+ *
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+			if (*s == accept[a])
+				return (s);
+		}
+		s++;
+	}
+	return (0)
+}
