fad6227e35b82680777dcb0861797872f04f79bd
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-25 17:18:29 -0700

    Diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - function to print two diagnoals of a square marix integer
+ * @a: input
+ * @size: input
+ *
+ * Return: 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int l_sum, r_sum, i;
+
+	l_sum = 0;
+	r_sum = 0;
+
+	for (i = 0; a < size; i++)
+	{
+		l_sum = l_sum + a[i * size + i];
+	}
+	
+	for (i = size - 1; i >= 0; i--)
+	{
+		r_sum += a[i * size + (size - y - 1)];
+	}
+	printf("%d, %d\n", l_sum, r_sum);
+}
