ceff27cdcdff41f13cd9c594c55fe0fed17d3259
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-27 15:32:57 -0700

    Length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * strlen_recursion - function that returns length of a string
+ * @s: input
+ *
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
+}
