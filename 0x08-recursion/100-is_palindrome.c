c35431a02aa640af72422cfed61f82c8542abb57
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-09-27 17:09:01 -0700

    Palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * is_palindrome - function to check if string is palindrome
+ * @s: input
+ *
+ * Return: 1 if palindrome else 0
+ */
+int is_palindrome(char *s)
+{
+	return (string_is_palindrome(s, _strlen_recursion(s) - 1, 0));
+}
+
+/**
+ * string_is_palindrome - check if string is a palindrome
+ * @s: input
+ * @n: input
+ * @i: input
+ * Return: 1 if palindrome, 0 if not
+ **/
+
+int string_is_palindrome(char *s, int n, int i)
+{
+	if (n <= i)
+		return (1);
+
+	if (s[i] != s[n])
+		return (0);
+
+	return (string_is_palindrome(s, n - 1, i + 1));
+}
+
+/**
+ * _strlen_recursion - function to return the length of string
+ * @s: input
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
