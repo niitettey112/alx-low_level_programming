383402ad3fdc4e0d6a9e89c8313c2d86b0579083
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-10-11 14:57:48 -0700

    a goal is not always

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,7 +4,7 @@
 #include "3-calc.h"
 /**
  * main - Prints the result of simple operations.
- * @argc: The number of arguments 
+ * @argc: The number of arguments
  * @argv: An array
  *
  * Return: 0
