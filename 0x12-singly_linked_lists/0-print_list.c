9807ab07fbf9e88bb1064d395cd36d89679a7ed4
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-10-19 16:31:11 -0700

    Print List

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,9 +14,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
 			h = h->next;
 			node_element++;
+		}
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		node_element++;
