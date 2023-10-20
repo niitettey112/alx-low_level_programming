71b1433eed78dbba430812cd1adac08480a60da9
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-10-19 17:54:28 -0700

    print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print - function to print before the main
+ *
+ * Return: void
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upone my back!\n");
+}
