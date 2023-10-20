68423c4e80b50d52b31565ef892656e2c588a073
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-10-19 17:49:32 -0700

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,13 +8,13 @@
  */
 void free_list(list_t *head)
 {
-	list t *tmp;
+	list_t *temp;
 
 	while (head != NULL)
 	{
-		tmp = head;
-		free(tmp->str);
+		temp = head;
+		free(temp->str);
 		head = head->next;
-		free(tmp);
+		free(temp);
 	}
 }
