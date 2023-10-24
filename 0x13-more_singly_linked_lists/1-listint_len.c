02c8f5f491ff5c39bc6fb9c3412241b512c1dc44
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-10-24 15:24:01 -0700

    Fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,8 +11,10 @@ size_t listint_len(const listint_t *h)
 	size_t count_num_node = 0;
 
 	while (h != NULL)
+	{
+		count_num_node += 1;
 		h = h->next;
-	count_num_node++;
+	}
 
 	return (count_num_node);
 }
