b2a8e374ca3092bddeca89d7a69bc346d977333b
Emmanuel Tetteh <tettehemmanuel112@gmail.com>
2023-10-24 10:40:41 -0700

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all elements of a listint_t list
+ * @h: parameter
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count_n = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = head->next;
+		count_n++;
+	}
+	return count_n;
+}
